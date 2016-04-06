5380cbb91c3a58485661275e1556c18bc7978c3e
tiao <tiao564@hotmail.com>
2016-04-06 13:39:12 -0400

    Dylan Guldy auger algorithm. First commit.
    My take on the algorithm design. This is the first
    iteration and many of the variables will be changed in testing.

--- project/main.c
+++ project/main.c
@@ -1,97 +1,327 @@
-/*Logan Friedman
-
- *Main Algorithm for soil sampling mechanisms. Calls functiosn from algorithm.c
- */
-
-
+//main file
 #define F_CPU 8000000UL
 
-#include "algorithm.h"
+/* Accel, for tilt and movement */
+#include "accelerometer_driver.h"
+
+/* Gyro for tilt */
+//#include "gyro_driver.h"
+
+/* i2c for gyro and accel coms to ATMEGA */
+#include "i2c_lib.h"
+
+/* Ultrasonic for object detection, progress tracking and tilt detection */
+#include "ultrasonic.h"
+
+/* Motor driver to operate motors */
+#include "motor.h"
+
+/* Encoder for progress tracking */
+#include "encoder.h"
+
+/* Vibration for danger detection */
+#include "vibration.h"
+
+/* Standard Avr library */
 #include <avr/io.h>
-#include <stdbool.h>
+
+/* Delay fucntions for timing */
 #include <util/delay.h>
-#include <stdint.h>
+
+/* Interrupts for AVR */
+#include <avr/interrupt.h>
+
 
 int main()
 {
-	//Busy wait for enable signal from user
-	check_enable();
+  /*******************
+   * Initializations *
+   ******************/
+  /* Set interrupt handler */
+  sei();
 
-	//Initialize intermediate variables
-	bool exit_code = COMPLETE;
-	bool obstacle;
-	bool direction = DOWN; 
-	bool motor_flag;
-	bool init_code = initialize();
-	bool sensor_chk;
-	uint16_t rotat_cnt = 0;
-	uint16_t trans_cnt = 0;
-	
-	//If initialization fails, returns error to user
-	if(init_code == INIT_FAILED)
-	{
-		exit_code = ERROR;
-	}
-	else
-	{
-		while(exit_code != ERROR)
-		{
-			//Checks if it is able to start digging
-			obstacle = check_obstacle_sensors();
-			if(obstacle == OBSTACLE_SENSOR_FAIL)
-			{
-				exit_code = ERROR;
-				break;
-			}
+  /* Abortion flag */
+  bool abort = false;
 
-			//drill begins downward motion turning to the right, starts the encoders
-			if(motor_flag == MOTOR_OFF && direction == DOWN)
-			{
-				start_motors_down();
-				init_encoders();
-				motor_flag = MOTOR_ON;
-			}
+  /**************
+   * Accel Init *
+   *************/
+  /* Accelerometer data struct */
+  accel_data a_data;
+  a_data.x = 0;
+  a_data.y = 0;
+  a_data.z = 0;
+  
+  /*Init accel*/
+  bool accel_init = init_accel();
+  if ( accel_ini != ACCEL_INIT_SUCCESS )
+  {
+    //Failed to init
+    abort = true;
+  }
+  
+  /*************
+   * Gyro Init *
+   ************/
+  /* Gyro data struct */
+//  gyro_data g_data;
+//  g_data.x = 0;
+//  g_data.y = 0;
+//  g_data.z = 0;
+//
+//  /*Full-scale range value*/
+//  gyro_range rng = RANGE_245_DPS;
+//
+//  /*Init gyro*/
+//  bool gyro_init = init_gyro(rng);
+//  if(gyro_init != GYRO_INIT_SUCCESS)
+//  {
+//    abort = true;
+//  }
 
-			//While drilling down, checking sensors and getting encoder counts to keep track of distance
-			//Stops motors at the appropriate time, or if there is an error 
-			if(motor_flag == MOTOR_ON && direction == DOWN)
-			{
-				while(trans_cnt < SIX_INCHES)
-				{
-					trans_cnt = get_trans_encoder_cnt();
-					sensor_chk = check_sensors();
-					if(sensor_chk == ERROR)
-					{
-						stop_motors();
-						motor_flag == MOTOR_OFF;
-						exit_code = ERROR;
-					}
-				}
-				stop_motors();
-				motor_flag = MOTOR_OFF;
-				stop_clear_encoders();
-				direction = UP;
-			}
+  /*******************
+   * Ultrasonic Init *
+   ******************/
+  init_ultrasonic_sensors();
 
-			//Pulls drill back up and turns off at 6 inches
-			if(motor_flag == MOTOR_OFF && direction == UP)
-			{
-				init_encoders();
-				start_motors_up();
-				while(trans_cnt< SIX_INCHES)
-				{
-					trans_cnt = get_trans_encoder_cnt();
-				}
-				stop_motors();
-				motor_flag = MOTOR_OFF;
-				stop_clear_encoders();
-				direction = DOWN;
-				break;
-			}
-		}
-	}
+  /**************
+   * Motor Init *
+   *************/
+  #define DUTY_CYCLE 128
+  init_motor_drivers();
+  brake_rotational_motor();
+  brake_translational_motor();
+  set_rotational_motor_speed(DUTY_CYCLE);
+  set_translational_motor_speed(DUTY_CYCLE);
 
-	//Sends signal to user to show completion or error occurred, disables everything
-	send_signal_and_disable(exit_code);
-	return 0;
-}
+  /****************
+   * Encoder Init *
+   ***************/
+  #define SAMPLE_RATE 200
+  set_sampling_rate(SAMPLE_RATE);
+  init_encoders();
+  clear_rotat_encoder_cnt();
+  clear_trans_encoder_cnt();
+
+  /*******************
+   * Vibrations Init *
+   ******************/
+  init_vibration_sensors();
+
+  /***********************
+   * End Initialiaztions *
+   **********************/
+
+
+
+  /***************************
+   * Varialbes for Algorithm *
+   **************************/
+  /* Algorithm Definitions */
+  //  MOST OF THESE ARE UNKNOWN //
+  #define ULTRASONIC_LIMIT    200
+  #define ULTRA_OFFSET_LIMIT  50
+  #define DISTANCE_TO_GROUND  400
+  #define GYRO_LIMIT          200
+  #define ACCEL_LIMIT         200
+  #define MIN_MOVE_DISTANCE   200
+  #define ENCODE_DONE         2000
+
+  /* Variables */
+  
+  //Abortion flag
+  abort = false;
+
+  //Accel variables
+  bool accel_status = read_accel(&a_data);
+
+  //Gyro variables
+  //???
+
+  //Ultrasonic variables
+  accum distanceA = get_obstacle_distance_cm(A);
+  accum distanceB = get_obstacle_distance_cm(B);
+  uint8_t AmB_dist;
+  uint8_t BmA_dist;
+  uint8_t old_dist_A;
+  uint8_t fail_cnt = 0;
+
+  //Encoder variables 
+  uint16_t trans_cnt = 0;
+  uint16_t old_r_cnt = 0;
+  uint16_t old_t_cnt = 0;
+ 
+  /*****************
+   * End Variables *
+   ****************/
+
+
+  /*******************
+   * Begin Algorithm *
+   ******************/
+
+  /*******************
+   * Start up checks *
+   ******************/
+
+  /*******************************************
+   * If not correct distance to ground abort *
+   ******************************************/
+  if( get_obstacle_distance_cm(A) < DISTANCE_TO_GROUND || get_obstacle_distance_cm(B) < DISTANCE_TO_GROUND ) 
+  {
+    abort = true;
+  } 
+
+  /****************************
+   * If tilted too much abort *
+   ***************************/
+  if( accel_status != ACCEL_READ_ERR )
+    {
+    if( a_data.x > ACCEL_LIMIT || a_data.y > ACCEL_LIMIT)
+    {
+      abort = true; 
+    }
+  }
+  ////////////////
+  //Keep this???//
+  ////////////////
+  if( accel_status == ACCEL_READ_ERR )
+  {
+    abort = true;
+  }
+
+  /****************************
+   * If tilted too much abort *
+   ***************************/
+//  if( g_data.x > GYRO_LIMIT || g_data.y > GYRO_LIMIT )
+//  {
+//    abort = true;
+//  }
+
+  /*************
+   * Motors on *
+   ************/
+  if(abort == false)
+  {
+    rotational_motor_right();   //MIGHT BE LEFT
+    translational_motor_down();
+  }
+
+  /*****************************************
+   * While not abort and not done sampling *
+   ****************************************/
+  while( (get_trans_encoder_cnt() < ENCODE_DONE) && (abort == false) )
+  {
+    /*********************
+     * Utlrasonic checks *
+     ********************/
+    //We are not moving
+    if( (old_dist_A-get_obstacle_distance_cm(A) ) < ULTRASONIC_LIMIT )    
+    {
+      fail_cnt = fail_cnt+1;
+    }
+    else
+    {
+      fail_cnt = 0; 
+    }
+    old_dist_A = get_obstacle_distance_cm(A);
+
+    //We are still not moving, abort
+    if(fail_cnt >= 3)
+    {
+      abort = true;
+    }
+    
+    //Use distance sensors to read tilt
+    AmB_dist = get_obstacle_distance_cm(A) - get_obstacle_distance_cm(B);
+    BmA_dist = get_obstacle_distance_cm(B) - get_obstacle_distance_cm(A);
+
+    if(AmB_dist > ULTRA_OFFSET_LIMIT && AmB_dist < 200)
+    {
+      abort = true;
+    }
+
+    if( BmA_dist > ULTRA_OFFSET_LIMIT && BmA_dist < 200)
+    {
+      abort = true;
+    }
+
+    /****************
+     * Accel Checks *
+     ***************/
+    /****************************
+    * If tilted too much abort *
+    ***************************/
+    if( a_data.x > ACCEL_LIMIT || a_data.y > ACCEL_LIMIT)
+    {
+      abort = true; 
+    }
+
+    /***************
+     * Gyro Checks *
+     **************/
+    /****************************
+     * If tilted too much abort *
+     ***************************/
+//`    if( g_data.x > GYRO_LIMIT || g_data.y > GYRO_LIMIT )
+//    {
+//      abort = true;
+//    }
+
+    /******************
+     * Encoder Checks *
+     *****************/
+    //We're not spinning
+    if( get_rotat_encoder_cnt() == old_r_cnt || get_trans_encoder_cnt() == old_t_cnt )
+    {
+      abort = true; 
+    }
+    old_r_cnt = get_rotat_encoder_cnt();
+    old_t_cnt = get_trans_encoder_cnt();
+
+    /********************
+     * Vibration Checks *
+     *******************/
+    if( check_vs_med_a() && check_vs_med_b() )
+    {
+      abort = true; 
+    }
+
+  }
+
+  /****************
+   * Brake motors *
+   ***************/
+  brake_rotational_motor();
+  brake_translational_motor();
+ 
+  /***********************
+   * Get encode position *
+   **********************/
+  trans_cnt = get_trans_encoder_cnt();
+  clear_trans_encoder_cnt();
+
+  /************************
+   * Turn signal light on *
+   ***********************/
+  if(abort == true)
+  {
+    //turn led on?? PORTX |= (1 << led_abort_port)
+  }
+
+  if(get_trans_encoder_cnt() >= ENCODE_DONE)
+  {
+    //turn led on?? PORTX |= (1 << led_done_port) 
+  }
+  /***********************
+   * bring drill back up *
+   **********************/
+  while( get_trans_encoder_cnt() != trans_cnt )
+  {
+    translational_motor_up();     
+  }
+
+  brake_translational_motor();
+
+  return 0;
+} 
